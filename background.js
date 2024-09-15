d2a9ccc28a3f9e2f3e377bd34dbc46eb64c87459
Andrey Prodromov <andreyprodromov@gmail.com>
2024-09-15 17:28:49 +0300

    Add files via upload

--- background.js
+++ background.js
@@ -1,13 +1,49 @@
-chrome.action.onClicked.addListener(handler);
+(async () => {
+    let state;
+    let ownedTabs;
 
-function handler(tab) {
-    chrome.tabs.create({url: "https://wikipedia.org"}, exec);
-}
+    await chrome.storage.session.get(["state", "ownedTabs"], data => {
+        const {'state': s, 'ownedTabs': t} = data;
 
-function exec(tab) {  
-    chrome.scripting.executeScript({
-        target : {tabId : tab.id, allFrames : true},
-        files : ["foreground.js"]
+        state = s || {};
+        ownedTabs = new Set(t);
     });
-    
-}
+
+    chrome.action.onClicked.addListener(() => {
+        chrome.tabs.create({url: "https://wikipedia.org", active: false}, tab => ownedTabs.add(tab.id))
+    });
+
+    chrome.webNavigation.onCompleted.addListener(
+        details => {
+            const id = details.tabId
+            if (ownedTabs.has(id)) {
+                exec(id);
+            }
+        }
+    );
+
+    function exec(id) {
+        state[id] = state[id] || 0;
+
+        switch (state[id]++) {
+            case 0:
+                chrome.scripting.executeScript({
+                    target : {tabId : id},
+                    files : ["foreground.js"]
+                });
+                break;
+        
+            case 1:
+                chrome.scripting.executeScript({
+                    target : {tabId : id},
+                    files : ["alert.js"]
+                });
+                break;
+        }
+
+        chrome.storage.session.set({
+            state: state,
+            ownedTabs: [...ownedTabs]
+        });
+    }
+})();
